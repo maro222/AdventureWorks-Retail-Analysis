e3d8b7c7c0ae37c276b1aab7e2780b1fb887c813
Omar Ayman <96844139+maro222@users.noreply.github.com>
2024-11-05 16:31:12 +0200

    Update main.py

--- streamlit_APP/main.py
+++ streamlit_APP/main.py
@@ -5,6 +5,7 @@ st.set_page_config(page_title="Customer Insights Dashboard", layout="wide")
 
 from app import run  # Import the function for the Customer Segmentations app
 from reseller_churn_app import app2  # Import the reseller churn prediction app
+from recommendations_app import recomendations_app
 
 # Set the sidebar title for navigation
 st.sidebar.title("Navigation")
