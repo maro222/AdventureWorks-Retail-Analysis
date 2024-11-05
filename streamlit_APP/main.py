d12b6b88233590a4d98d97edd97d2a4a1bcd09c3
Omar Ayman <96844139+maro222@users.noreply.github.com>
2024-11-05 17:24:28 +0200

    Update main.py

--- streamlit_APP/main.py
+++ streamlit_APP/main.py
@@ -5,7 +5,7 @@ st.set_page_config(page_title="Customer Insights Dashboard", layout="wide")
 
 from app import run  # Import the function for the Customer Segmentations app
 from reseller_churn_app import app2  # Import the reseller churn prediction app
-from recommendations_app import recomendations_app
+# from recommendations_app import recomendations_app
 
 # Set the sidebar title for navigation
 st.sidebar.title("Navigation")
@@ -14,10 +14,10 @@ st.sidebar.title("Navigation")
 page = st.sidebar.selectbox("Select a Page", ["Customer Recommendations", "Customer Segmentations", "Reseller Churn"])
 
 # Run the selected app with added visuals and interactivity
-if page == "Customer Recommendations":
-    recommendations_app()
-elif page == "Customer Segmentations":
+if page == "Customer Segmentations":
     run()
+# elif page == "Customer Recommendations":
+#     recommendations_app()
 else:
     app2()
 
