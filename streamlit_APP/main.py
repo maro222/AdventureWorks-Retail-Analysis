de05da592b4016f2977162fa5e0167145053e3f7
Omar Ayman <96844139+maro222@users.noreply.github.com>
2024-11-05 14:54:20 +0200

    Update main.py

--- streamlit_APP/main.py
+++ streamlit_APP/main.py
@@ -1,9 +1,10 @@
 import streamlit as st
-from app import run  # Import the function for the Customer Segmentations app
-from reseller_churn_app import app2  # Import the reseller churn prediction app
 
 # Set the page title and layout - this must be the first Streamlit command
 st.set_page_config(page_title="Customer Insights Dashboard", layout="wide")
+
+from app import run  # Import the function for the Customer Segmentations app
+from reseller_churn_app import app2  # Import the reseller churn prediction app
 
 # Set the sidebar title for navigation
 st.sidebar.title("Navigation")
@@ -16,4 +17,5 @@ if page == "Customer Segmentations":
     run()
 else:
     app2()
+
 
