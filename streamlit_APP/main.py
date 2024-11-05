816d262c8567c04f2061c68fe9b57d4e114fe7d4
Omar Ayman <96844139+maro222@users.noreply.github.com>
2024-11-05 16:27:37 +0200

    Update main.py

--- streamlit_APP/main.py
+++ streamlit_APP/main.py
@@ -10,10 +10,12 @@ from reseller_churn_app import app2  # Import the reseller churn prediction app
 st.sidebar.title("Navigation")
 
 # Select app section
-page = st.sidebar.selectbox("Select a Page", ["Customer Segmentations", "Reseller Churn"])
+page = st.sidebar.selectbox("Select a Page", ["Customer Recommendations", "Customer Segmentations", "Reseller Churn"])
 
 # Run the selected app with added visuals and interactivity
-if page == "Customer Segmentations":
+if page == "Customer Recommendations":
+    recommendations_app()
+elif page == "Customer Segmentations":
     run()
 else:
     app2()
